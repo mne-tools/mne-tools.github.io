f0e0cdefc7baba3a0d6e18d8687756012db11f85
Circle Ci <circle@mne.com>
2016-10-11 17:17:13 +0000

    Automated update of dev docs.

--- dev/_downloads/plot_brainstorm_data.py
+++ dev/_downloads/plot_brainstorm_data.py
@@ -34,12 +34,12 @@ data_path = bst_raw.data_path()
 
 raw_fname = data_path + '/MEG/bst_raw/' + \
                         'subj001_somatosensory_20111109_01_AUX-f_raw.fif'
-raw = mne.io.read_raw_fif(raw_fname, preload=True, add_eeg_ref=False)
+raw = mne.io.read_raw_fif(raw_fname, preload=True)
 raw.plot()
 
 # set EOG channel
 raw.set_channel_types({'EEG058': 'eog'})
-raw.add_eeg_average_proj()
+raw.set_eeg_reference()
 
 # show power line interference and remove it
 raw.plot_psd()
