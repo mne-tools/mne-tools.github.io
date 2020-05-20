726a4467af624c7a06f7ec0ebefbd529c4f7437b
Daniel McCloy <dan@mccloy.info>
2020-05-20 11:53:36 -0700

    Apply suggestions from hoechenberger
    Co-authored-by: Richard Höchenberger <richard.hoechenberger@gmail.com>

--- versionwarning.js
+++ versionwarning.js
@@ -1,12 +1,12 @@
 (function() {
     // adapted 2020-05 from https://scikit-learn.org/versionwarning.js
-    var latestStable = '0.20.5';
-    var goodPaths = ['stable', 'dev', latestStable];
-    var showWarning = (msg) => {
+    const latestStable = '0.20.5';
+    const goodPaths = ['stable', 'dev', latestStable];
+    const showWarning = (msg) => {
         $('.body[role=main]').prepend(`<div class="d-block devbar alert alert-danger">${msg}</div>`)
     };
-    if (location.hostname == 'mne.tools' || location.hostname.includes('circle-artifacts.com')) {
-        var versionPath = location.pathname.split('/')[1];
+    if (location.hostname === 'mne.tools' || location.hostname.includes('circle-artifacts.com')) {
+        const versionPath = location.pathname.split('/')[1];
         if (!goodPaths.includes(versionPath)) {
             const warning = `This is documentation for an old release of MNE-Python (version ${versionPath}).
             Try the <a href="https://mne.tools">latest stable release</a> (version ${latestStable})
