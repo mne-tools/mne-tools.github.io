42c58677f9d32b658b21e6d4307ed183fb3860eb
Daniel McCloy <dan@mccloy.info>
2020-05-20 11:59:04 -0700

    Create versionwarning.js (#19)
    * Create versionwarning.js
    
    * Apply suggestions from hoechenberger
    
    Co-authored-by: Richard Höchenberger <richard.hoechenberger@gmail.com>
    
    * minor (not patch); devbar in template for dev site
    
    Co-authored-by: Richard Höchenberger <richard.hoechenberger@gmail.com>

---
+++ versionwarning.js
@@ -0,0 +1,17 @@
+(function() {
+    // adapted 2020-05 from https://scikit-learn.org/versionwarning.js
+    const latestStable = '0.20';
+    const goodPaths = ['stable', 'dev', latestStable];
+    const showWarning = (msg) => {
+        $('.body[role=main]').prepend(`<div class="d-block devbar alert alert-danger">${msg}</div>`)
+    };
+    if (location.hostname === 'mne.tools' || location.hostname.includes('circle-artifacts.com')) {
+        const versionPath = location.pathname.split('/')[1];
+        if (!goodPaths.includes(versionPath)) {
+            const warning = `This is documentation for an old release of MNE-Python (version ${versionPath}).
+            Try the <a href="https://mne.tools">latest stable release</a> (version ${latestStable})
+            or the <a href="https://mne.tools/dev">development</a> (unstable) version.`;
+            showWarning(warning)
+        }
+    }
+})()
