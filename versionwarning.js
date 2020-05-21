b2e12c970ac4bc7e74f7700832697d109cd07ad1
Daniel McCloy <dan@mccloy.info>
2020-05-21 08:42:32 -0700

    embed style into JS-written devbar (#21)
    * embed style into JS-written devbar
    
    * remove circleCI from conditional

--- versionwarning.js
+++ versionwarning.js
@@ -2,15 +2,26 @@
     // adapted 2020-05 from https://scikit-learn.org/versionwarning.js
     const latestStable = '0.20';
     const goodPaths = ['stable', 'dev', latestStable];
+    const devbar_style = [
+        'text-align: center',
+        'padding: 5px',
+        'margin-bottom: 5px',
+        'border-radius: 0 0 4px 4px !important',
+        'background-color: #e74c3c',
+        'border-color: #e74c3c',
+        'color: #ffffff',
+    ].join('; ')
     const showWarning = (msg) => {
-        $('body').prepend(`<div class="d-block devbar alert alert-danger">${msg}</div>`)
+        $('body').prepend(`<div class="d-block devbar alert alert-danger" style="${devbar_style}">${msg}</div>`)
+
     };
-    if (location.hostname === 'mne.tools' || location.hostname.includes('circle-artifacts.com')) {
+    if (location.hostname === 'mne.tools') {
         const versionPath = location.pathname.split('/')[1];
         if (!goodPaths.includes(versionPath)) {
+            const link_style = "color: #ffffff; font-weight: bold"
             const warning = `This is documentation for an old release of MNE-Python (version ${versionPath}).
-            Try the <a href="https://mne.tools">latest stable release</a> (version ${latestStable})
-            or the <a href="https://mne.tools/dev">development</a> (unstable) version.`;
+            Try the <a style="${link_style}" href="https://mne.tools">latest stable release</a> (version ${latestStable})
+            or the <a style="${link_style}" href="https://mne.tools/dev">development</a> (unstable) version.`;
             showWarning(warning)
         }
     }
