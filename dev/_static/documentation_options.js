20eeada46203f4eb91a65a510528048aa5ef9aa4
Circle CI <circle@mne.com>
2023-04-11 09:03:34 +0000

    CircleCI update of dev docs (53825).

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.4.0.dev107+ga458622f4',
+    VERSION: '1.4.0.dev108+gb25f25057',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
