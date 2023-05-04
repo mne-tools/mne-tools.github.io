9364b15db28aab92139da134d9044f78a0804274
Daniel McCloy <dan@mccloy.info>
2023-05-03 22:40:46 -0500

    purge jquery; automate checking against stable (#27)

--- versionwarning.js
+++ versionwarning.js
@@ -1,35 +1,55 @@
-(function() {
+function showVersionWarning() {
     // adapted 2020-05 from https://scikit-learn.org/versionwarning.js
     if (location.hostname === 'mne.tools') {
         const urlParts = location.pathname.split('/');
         const version = urlParts[1];
+        const releases = Array.from(
+            document.querySelector('.version-switcher__menu').children
+        );
+        const latest_stable = parseFloat(
+            releases.filter(
+                ver => ver.getAttribute('data-version') == 'stable'
+            )[0].getAttribute('data-version-name').split(' ')[0]
+        );
         // see if filePath exists in the stable version of the docs
         var filePath = urlParts.slice(2).join('/');
-        $.ajax({
-            type: 'HEAD',
-            url: `https://mne.tools/stable/${filePath}`,
-            error: function() {
+
+        fetch(`https://mne.tools/stable/${filePath}`, { method: "HEAD" })
+        .then((response) => {
+            if (!response.ok) {
                 filePath = '';
-            },
-            complete: function() {
-                if (version !== 'stable') {
-                    // parse version to figure out which website theme classes to use
-                    var pre = '<div class="container-fluid alert-danger devbar"><div class="row no-gutters"><div class="col-12 text-center">';
-                    var post = '</div></div></div>';
-                    var anchor = 'class="btn btn-danger font-weight-bold ml-3 my-3 align-baseline"';
-                    if (parseFloat(version) < 0.23) {  // 'dev' → NaN → false (which is what we want)
-                        pre = '<div class="d-block devbar alert alert-danger">';
-                        post = '</div>';
-                        anchor = 'class="btn btn-danger" style="font-weight: bold; vertical-align: baseline; margin: 0.5rem; border-style: solid; border-color: white;"';
-                    }
-                    // triage message
-                    var verText = `an <strong>old version (${version})</strong>`;
-                    if (version == 'dev') {
-                        verText = 'the <strong>unstable development version</strong>';
-                    }
-                    $('body').prepend(`${pre}This is documentation for ${verText} of MNE-Python. <a ${anchor} href="https://mne.tools/stable/${filePath}">Switch to stable version</a>${post}`);
-                }
             }
-        });
+        })
+        .then(() => {
+            if (version !== 'stable') {
+                const outer = document.createElement("div", { "class": "bd-header-announcement container-fluid" });
+                const middle = document.createElement("div", { "class": "bd-header-announcement__content" });
+                const inner = document.createElement("div", { "class": "sidebar-message" });
+                const bold = document.createElement("strong");
+                outer.appendChild(middle);
+                middle.appendChild(inner);
+                // for less-than comparison: 'dev' → NaN → false (which is what we want)
+                inner.innerText = "This is documentation for";
+                if (parseFloat(version) < latest_stable) {
+                    inner.innerText += "an"
+                    bold.innerText = `old version (${version})`;
+                } else {
+                    inner.innerText += "the"
+                    bold.innerText = "unstable development version";
+                }
+                inner.appendChild(bold);
+                inner.appendChild(document.createTextNode("of MNE-Python."))
+                const anchor = document.createElement("a", {
+                    "class": "sd-btn sd-btn-danger sd-shadow-sm sd-text-wrap font-weight-bold ms-3 my-3 align-baseline",
+                    "href": `https://mne.tools/stable/${filePath}` },
+                    "Switch to stable version");
+                inner.appendChild(anchor);
+                const refNode = document.querySelector('.bd-header');
+                insertBefore(newNode, refNode);
+            }
+        })
     }
-})()
+}
+
+if (document.readyState != "loading") showVersionWarning();
+else document.addEventListener("DOMContentLoaded", showVersionWarning);
