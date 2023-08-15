533d4fddf522bf7fc0e4768f5b35936ca81e12f2
Eric Larson <larson.eric.d@gmail.com>
2023-08-15 12:13:27 -0400

    1.5 Squash

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.4.0.dev148+g425e3b344',
+    VERSION: '1.5.0.dev6+g97db26cb8',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
