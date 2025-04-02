7875c694c692eaadbba1b1fc1a70b194225f5331
Circle CI <circle@mne.tools>
2025-04-02 07:15:35 +0000

    CircleCI update of dev docs (72372).

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.10.0.dev77+g704b3930e',
+    VERSION: '1.10.0.dev78+ge65a55965',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
