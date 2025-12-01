9d77c2cb10e6add5781ce6ae20755a3b61f503f8
Circle CI <circle@mne.tools>
2025-12-01 18:19:36 +0000

    CircleCI update of mne-gui-addons docs (284).

--- mne-gui-addons/_static/documentation_options.js
+++ mne-gui-addons/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.2.0.dev29+ge16d6fe27',
+    VERSION: '0.2.0.dev30+gbf26ba667',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
