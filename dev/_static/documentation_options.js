237960515bad6e0f9462b9384b0260f1b668550a
Circle CI <circle@mne.tools>
2026-09-09 22:05:12 +0000

    CircleCI deploy of dev docs (87873).
    Deployed-version: dev
    Source-sha: e54d799846754a6cf0ec483122933a14c8b65ecf

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.14.0.dev5+gdb685d654',
+    VERSION: '1.14.0.dev7+ge54d79984',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
